4d359b7b629c94b738ffe0e8d887be92e03bc758
Mawar Dhita <149513147+mawdhita@users.noreply.github.com>
2024-03-21 15:51:51 +0700

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.querySelector('.').addEventListener('click', function() {
+    document.querySelector('#popup').style.display = 'block';
+   });
+   
+   document.querySelector('.popup-tutup').addEventListener('click', function() {
+    document.querySelector('#popup').style.display = 'none';
+   });
